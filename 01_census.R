aca5aa4202ec3ec0f0a7e0a2c14fd186bbea08de
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-12-10 22:39:10 -0800

    format edit

--- 01_census.R
+++ 01_census.R
@@ -11,11 +11,15 @@ library("tidycensus")
 library("dplyr")
 library("ggplot2")
 
+
+# video1: overview --------------------------------------------------------
+
 # access from 1990, 2000, 2010 (default) census
   # geography - specifies level of aggregation
   # variables - vector of census variable ID codes
 state_pop <- get_decennial(geography = "state",
                            variables = "P001001")
+head(state_pop)
 
 # get_ACS for American Community Survey
     # default - 5 year 2012-2016 ACS sample
@@ -23,3 +27,7 @@ state_pop <- get_decennial(geography = "state",
         # rather than exact data, & thus characterized by Margins of Error 
 state_income <- get_acs(geography = "state",
                         variables = "B19013_001")
+head(state_income)
+
+
+
